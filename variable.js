9323b8aa49ff1cd68711b406070deefc8485c86b
Tower59 <g.thompson59@live.co.uk>
2021-02-05 03:06:55 +0000

    cont.

--- variable.js
+++ variable.js
@@ -9,3 +9,8 @@ var camelCode = "therapist.com";
 // string concatenation //
 
 alert("hello" + "World");
+
+// more string concatenation //
+var tweet = prompt("Compose your tweet");
+var tweetCount = tweet.length;
+alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining")
