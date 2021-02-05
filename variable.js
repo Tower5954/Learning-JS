3ded7fb21d7e49a2f6c4b64c38beeddfd81bc3da
Tower59 <g.thompson59@live.co.uk>
2021-02-05 03:01:58 +0000

    first commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+var myName = "Grant";
+var yourName = "Angela";
+var myvar = "abc";
+var my123 = 123;
+var myname = "Grant";
+var my_name = "Grant";
+var camelCode = "therapist.com";
+
+// string concatenation //
+
+alert("hello" + "World");
